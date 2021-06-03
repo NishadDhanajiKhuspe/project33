d2bdf0e1e25b40a6bbe5f3d2a993e3eb252ecb68
NishadDhanajiKhuspe <80624824+NishadDhanajiKhuspe@users.noreply.github.com>
2021-06-03 13:58:41 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+ var backGround,backGroundImg,backGroundImage;
+function preload(){
+  backGroundImg =loadImage("snow2.jpg");
+  backGroundImage =loadImage("snow3.jpg")
+}
+function setup() {
+  createCanvas(4000,1000);
+  backGround = createSprite(400, 200, 1000, 4000);
+  backGround.addImage(backGroundImg);
+  
+}
+
+function draw() {
+  background(255,255,255); 
+   keyPressed();
+  drawSprites();
+}
+function keyPressed(){
+  if(keyPressed === 32){
+    backGround.changeImage(backGroundImage);
+  }
+}
